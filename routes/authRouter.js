4a2e408822f8ce8628a9eff0feddc34c8d36a628
Anish Tiwari <anish.at.tiwari@gmail.com>
2023-06-10 23:39:39 +0530

    feat: added changeusername and signup api with password hashing

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const User = mongoose.model('User');
-
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const nodemailer = require('nodemailer');
 
@@ -45,18 +45,43 @@ router.post('/verify', (req, res) => {
                 );
                 await mailer(email, VerificationCode);
 
-                return res
-                    .status(200)
-                    .send({
-                        message: 'Verification code sent to your email',
-                        VerificationCode,
-                        email,
-                    });
+                return res.status(200).send({
+                    message: 'Verification code sent to your email',
+                    VerificationCode,
+                    email,
+                });
             } catch (err) {
                 return res.status(422).send({ error: 'Error sending email' });
             }
         });
     }
 });
-
+router.post('/changeusername', (req, res) => {
+    const { username } = req.body;
+    User.find({ username }).then(async (savedUsername) => {
+        if (savedUsername.length > 0) {
+            return res.status(422).send({ error: 'Username already exists' });
+        } else {
+            return res
+                .status(200)
+                .send({ message: 'Username available', username });
+        }
+    });
+});
+router.post('/signup', async (req, res) => {
+    const { email, username, password } = req.body;
+    if (!email || !username || !password) {
+        return res.status(422).send({ error: 'Please enter all the fields' });
+    } else {
+        const user = new User({ username, email, password });
+        try {
+            await user.save();
+            //save the user to the db
+            const token = jwt.sign({_id: user._id}, process.env.JWT_SECRET);
+            return res.status(200).send({message: 'User registered successfully', token})
+        } catch (err) {
+            return res.status(422).send({error: err})
+        }
+    }
+});
 module.exports = router;
